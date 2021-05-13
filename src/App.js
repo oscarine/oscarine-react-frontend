d82a64dead17cfc9c80a726a9921133c6ef144aa
Ashraf Mahmood <55829865+ashrafmahmood@users.noreply.github.com>
2021-05-13 23:57:22 +0530

    feat: Add Shop Search & Layout components (#13)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './components/auth/Login'
 import SignUp from './components/auth/SignUp'
+import BottomNav from './components/Layouts/BottomNav'
+import Header from './components/Layouts/Header'
+import Home from './components/Home/Home'
+import Search from './components/Home/Search'
 
 function App () {
   return (
@@ -12,9 +16,17 @@ function App () {
         <Route path='/login'>
           <Login />
         </Route>
+        <Route path='/search'>
+          <Search />
+          <BottomNav />
+        </Route>
+        <Route path='/'>
+          <Header />
+          <Home />
+          <BottomNav />
+        </Route>
       </Switch>
     </Router>
-
   )
 }
 
