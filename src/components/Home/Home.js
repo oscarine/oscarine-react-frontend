808e12d3680e332355a509bf3c24ff6b6fe846a7
Ashraf Mahmood <mahmoodlko03@gmail.com>
2021-05-24 19:48:12 +0530

    feat: fetch shops list  from server

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BottomNav from '../Layouts/BottomNav'
 import Header from '../Layouts/Header'
 import Shop from './Shop'
+import axios from '../../axios'
+import { useStateValue } from '../../StateProvider'
 
 function Home () {
+  const [{ shops }, dispatch] = useStateValue()
+
+  const [latitude, setLatitude] = useState(null)
+  const [longitude, setLongitude] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+    })
+    if (longitude && latitude) {
+      shopsListGetRequest()
+    }
+  }, [latitude, longitude])
+
+  const shopsListGetRequest = async () => {
+    try {
+      const resp = await axios.get('/api/v1/shops-list', { params: { longitude: longitude, latitude: latitude } })
+      setLoading(false)
+      setError(null)
+      dispatch({
+        type: 'FETCH_SHOPS',
+        shopData: resp.data
+      })
+    } catch (err) {
+      setLoading(false)
+      setError(err.message)
+    }
+  }
+
   return (
     <div className='bg-gray-100 '>
       <Header />
       <div className='bg-gray-100 z-0 mt-16 pb-16  max-w-6xl m-auto p-2 md:p-6 grid gap-3 grid-cols-1 md:grid-cols-auto  place-items-center'>
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
-        <Shop />
+        {!loading
+          ? (shops.map((shop) => (<Shop
+              id={shop.id}
+              key={shop.id}
+              name={shop.name}
+              address={shop.address}
+              phone_number={shop.phone_number}
+                                  />)))
+          : <p>Loading shops near you...</p>}
+        {error ? (<p>{error}</p>) : null}
       </div>
       <BottomNav />
     </div>
--- src/components/Home/Shop.js
+++ src/components/Home/Shop.js
@@ -2,13 +2,13 @@ import { StarIcon } from '@heroicons/react/solid'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Shop () {
+function Shop ({ id, name, address, phone_number }) {
   return (
     <Link className=' mt-0 md:mt-5 lg:w-64  container mx-auto p-4 bg-white rounded-xl border hover:border-gray-300  flex flex-row md:flex-col  place-items-center md:transform hover:scale-105 ease-in-out duration-500' to='/shop-view'>
       <img alt='ShopImage' className='w-28 md:w-60 object-contain' src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rsqfmdbnv3w2zf5g7qdn' />
       <div className='w-full flex flex-col ml-4 md:ml-0  place-items-left'>
-        <h1 className='md:mt-3 font-medium text-black-500 text-lg'>Shop Name</h1>
-        <p className='text-gray-500 text-md -mt-1'>Description</p>
+        <h1 className='md:mt-3 font-medium text-black-500 text-lg'>{name}</h1>
+        <p className='text-gray-500 text-md -mt-1'>{address}</p>
         <div className='flex items-center bg-yellow-400 w-10 pl-1 pr-1 mt-3.5'>
           <StarIcon className='h-4 w-4 text-white ' />
           <p className='text-white text-sm'>3.6</p>
