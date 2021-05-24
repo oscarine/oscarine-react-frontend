cb2d8869586495f7788378e835b98030d860e180
Haider Ali <haider.lee23@gmail.com>
2021-05-24 17:26:47 +0530

    Add prop types

--- src/UI/Auxilliary.js
+++ src/UI/Auxilliary.js
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types'
+
 const Auxilliary = ({ className, ...props }) => {
   return (
     <div className={className}>
       {props.children}
     </div>
   )
+}
+
+Auxilliary.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.arrayOf(PropTypes.oneOfType(
+    [
+      PropTypes.element,
+      PropTypes.bool
+    ]
+  )).isRequired
 }
 
 export default Auxilliary
