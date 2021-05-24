4e61fa0d236860e57369d53881c75aeffa7b596f
Ashraf Mahmood <mahmoodlko03@gmail.com>
2021-05-24 21:12:06 +0530

    fix: Should not have unused variables

--- src/components/Home/Shop.js
+++ src/components/Home/Shop.js
@@ -2,7 +2,7 @@ import { StarIcon } from '@heroicons/react/solid'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Shop ({ id, name, address, phone_number }) {
+function Shop ({ name, address }) {
   return (
     <Link className=' mt-0 md:mt-5 lg:w-64  container mx-auto p-4 bg-white rounded-xl border hover:border-gray-300  flex flex-row md:flex-col  place-items-center md:transform hover:scale-105 ease-in-out duration-500' to='/shop-view'>
       <img alt='ShopImage' className='w-28 md:w-60 object-contain' src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rsqfmdbnv3w2zf5g7qdn' />
