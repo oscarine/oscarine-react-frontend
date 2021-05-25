88866ca40fa7ce620241bdf9cd0d2f79ac5ed654
Ashraf Mahmood <55829865+ashrafmahmood@users.noreply.github.com>
2021-05-26 00:48:37 +0530

    feat: Fetch shops list from server (#25)

--- src/App.js
+++ src/App.js
@@ -6,35 +6,39 @@ import Home from './components/Home/Home'
 import Search from './components/Home/Search'
 import ShopSearch from './components/ShopView/ShopSearch'
 import ShopView from './components/ShopView/ShopView'
+import { StateProvider } from './StateProvider'
+import reducer, { initialState } from './reducer'
 
 function App () {
   const shopSearch = (<div className='md:hidden'><ShopSearch /></div>)
   return (
-    <Router>
-      <Switch>
-        <Route path='/signup'>
-          <SignUp />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/search'>
-          <Search />
-        </Route>
-        <Route path='/shop-view'>
-          <ShopView />
-        </Route>
-        <Route path='/shop-search'>
-          {shopSearch}
-        </Route>
-        <Route path='/cart'>
-          <Cart />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Router>
+        <Switch>
+          <Route path='/signup'>
+            <SignUp />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/shop-view'>
+            <ShopView />
+          </Route>
+          <Route path='/shop-search'>
+            {shopSearch}
+          </Route>
+          <Route path='/cart'>
+            <Cart />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </StateProvider>
   )
 }
 
