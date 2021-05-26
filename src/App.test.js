4c32671d03c209b09b82bafcdbdbfa7d51af761f
Ashraf Mahmood <55829865+ashrafmahmood@users.noreply.github.com>
2021-05-26 23:57:32 +0530

    fix: Add reducer in Home and remove global reducer.js and StateProvider.js (#29)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import { render, cleanup } from '@testing-library/react'
 import React from 'react'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 afterEach(cleanup)
 
 test('renders react', () => {
-  render(<App />)
+  const div = document.createElement('div')
+  render(<BrowserRouter><App /></BrowserRouter>, div)
 })
