13747c27270e867981df14871808d16436005871
Haider Ali <haider.lee23@gmail.com>
2021-05-26 13:49:32 +0530

    refactor: Remove div render from App test (#28)

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,6 @@ import App from './App'
 
 afterEach(cleanup)
 
-test('renders learn react link', () => {
-  const div = document.createElement('div')
-  render(<App />, div)
+test('renders react', () => {
+  render(<App />)
 })
