83ef11ef686ed46e780d2c59a42ba6e4eaf661e3
Ashraf Mahmood <55829865+ashrafmahmood@users.noreply.github.com>
2021-05-27 14:44:40 +0530

    feat: Add loading UI in Home (#32)

--- src/components/Home/Shop.js
+++ src/components/Home/Shop.js
@@ -1,21 +1,41 @@
 import { StarIcon } from '@heroicons/react/solid'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-function Shop ({ name, address }) {
+function Shop ({ address, loading, name }) {
+  const linkDisableHandler = useCallback(
+    (event) => {
+      loading && event.preventDefault()
+    },
+    [loading]
+  )
+
   return (
-    <Link className=' mt-0 md:mt-5 lg:w-64  container mx-auto p-4 bg-white rounded-xl border hover:border-gray-300  flex flex-row md:flex-col  place-items-center md:transform hover:scale-105 ease-in-out duration-500' to='/shop-view'>
-      <img alt='ShopImage' className='w-28 md:w-60 object-contain' src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rsqfmdbnv3w2zf5g7qdn' />
+    <Link className={`mt-0 md:mt-5 lg:w-64  container mx-auto p-4 bg-white rounded-xl border ${!loading && 'hover:border-gray-300'}  flex flex-row md:flex-col  place-items-center md:transform ${!loading && 'hover:scale-105'} ease-in-out duration-500`} onClick={linkDisableHandler} to='/shop-view'>
+      <img alt='ShopImage' className={`w-28 md:w-60 object-contain ${loading && 'hidden'}`} src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rsqfmdbnv3w2zf5g7qdn' />
+      <div className={`w-44 h-20 md:w-56 md:h-32 bg-gray-200 animate-pulse rounded-sm ${!loading && 'hidden'}`} />
       <div className='w-full flex flex-col ml-4 md:ml-0  place-items-left'>
-        <h1 className='md:mt-3 font-medium text-black-500 text-lg'>{name}</h1>
-        <p className='text-gray-500 text-md -mt-1'>{address}</p>
-        <div className='flex items-center bg-yellow-400 w-10 pl-1 pr-1 mt-3.5'>
-          <StarIcon className='h-4 w-4 text-white ' />
-          <p className='text-white text-sm'>3.6</p>
+        <h1 className={`${!loading && 'md:mt-3 font-medium text-black-500 text-lg'} ${loading && 'md:mt-5 h-6 w-36 bg-gray-200 animate-pulse rounded-sm'}`}>{loading ? null : name}</h1>
+        <p className={`${!loading && 'text-gray-500 text-md -mt-1'} ${loading && 'mt-1 h-4 w-24 bg-gray-200 animate-pulse rounded-sm'}`}>{loading ? null : address}</p>
+        <div className={`${!loading && 'flex items-center bg-yellow-400 w-10 pl-1 pr-1 mt-3.5'} ${loading && 'mt-3.5 h-4 w-8 bg-gray-200 animate-pulse'}`}>
+          <StarIcon className={`h-4 w-4 text-white ${loading && 'hidden'}`} />
+          <p className={`text-white text-sm ${loading && 'hidden'}`}>3.6</p>
         </div>
       </div>
     </Link>
   )
+}
+
+Shop.propTypes = {
+  address: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  name: PropTypes.string
+}
+
+Shop.defaultProps = {
+  address: '',
+  name: ''
 }
 
 export default Shop
