0c6b02aa103072c9f1296dc3381d5b07b434aa50
Ashraf Mahmood <55829865+ashrafmahmood@users.noreply.github.com>
2021-05-29 20:17:34 +0530

    feat: Add ErrorModal component (#36)

--- src/components/Home/Shop.js
+++ src/components/Home/Shop.js
@@ -6,13 +6,15 @@ import PropTypes from 'prop-types'
 function Shop ({ address, loading, name }) {
   const linkDisableHandler = useCallback(
     (event) => {
-      loading && event.preventDefault()
+      if (loading) {
+        event.preventDefault()
+      }
     },
     [loading]
   )
 
   return (
-    <Link className={`mt-0 md:mt-5 lg:w-64  container mx-auto p-4 bg-white rounded-xl border ${!loading && 'hover:border-gray-300'}  flex flex-row md:flex-col  place-items-center md:transform ${!loading && 'hover:scale-105'} ease-in-out duration-500`} onClick={linkDisableHandler} to='/shop-view'>
+    <Link className={`mt-0 md:mt-5 lg:w-64  container mx-auto p-4 bg-white rounded-md md:rounded-lg border ${!loading && 'hover:border-gray-300'}  flex flex-row md:flex-col  place-items-center md:transform ${!loading && 'hover:scale-105'} ease-in-out duration-500`} onClick={linkDisableHandler} to='/shop-view'>
       <img alt='ShopImage' className={`w-28 md:w-60 object-contain ${loading && 'hidden'}`} src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rsqfmdbnv3w2zf5g7qdn' />
       <div className={`w-44 h-20 md:w-56 md:h-32 bg-gray-200 animate-pulse rounded-sm ${!loading && 'hidden'}`} />
       <div className='w-full flex flex-col ml-4 md:ml-0  place-items-left'>
