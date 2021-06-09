077000e045c7e1d60b016dd6f42fa3f1db5de8d1
Ashraf Mahmood <mahmoodlko03@gmail.com>
2021-06-09 19:08:33 +0530

    fix: replace localStorage with window.localStorage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,12 +1,12 @@
 
 export function getFromLocalStorage (key) {
-  return JSON.parse(localStorage.getItem(key))
+  return JSON.parse(window.localStorage.getItem(key))
 }
 
 export function setToLocalStorage (key, value) {
-  localStorage.setItem(key, JSON.stringify(value))
+  window.localStorage.setItem(key, JSON.stringify(value))
 }
 
 export function removeFromLocalStorage (key) {
-  localStorage.removeItem(key)
+  window.localStorage.removeItem(key)
 }
